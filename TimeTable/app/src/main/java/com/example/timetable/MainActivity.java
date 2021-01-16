98b3f4ae7bb6c1a7796c05c6e125f27409535a8a
vidhibadrakiya <201901434@daiict.ac.in>
2021-01-16 14:14:16 -0800

    This is Time Table app

---
+++ TimeTable/app/src/main/java/com/example/timetable/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.timetable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void b1(View v)
+    {
+
+    }
+
+    public void b2(View v)
+    {
+
+    }
+
+    public void b3(View v)
+    {
+
+    }
+
+    public void b4(View v)
+    {
+
+    }
+
+
+    public void b5(View v)
+    {
+
+    }
+
+    public void b6(View v)
+    {
+
+    }
+
+
+
+
+}
